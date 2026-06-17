18050244c2d190977cf30598793de2a1eb04584a
FongFox <phong.tgn.coder@gmail.com>
2026-06-17 09:54:21 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -9,11 +9,14 @@ const rl = createInterface({
 // TODO: Uncomment the code below to pass the first stage
 rl.prompt();
 rl.on('line', (command) => {
-  if(command == 'exit') {
+  if(command.startsWith('exit')) {
     rl.close();
     return;
+  } else if(command.startsWith('echo')) {
+    console.log(command.slice(5));
+  } else {
+    console.log(`${command}: command not found`);
   }
-  console.log(`${command}: command not found`);
   rl.prompt();
 });
 
