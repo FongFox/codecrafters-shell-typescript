a4ab11c74b893de1b5b654bddba96e1fb1c7d880
FongFox <phong.tgn.coder@gmail.com>
2026-06-22 20:42:04 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -10,6 +10,23 @@ const rl = createInterface({
     prompt: "$ ",
 });
 
+const findInPath = (name: string): [boolean, string] => {
+    const pathString: string = process.env.PATH ?? "";
+    const directories: string[] = pathString.split(path.delimiter);
+    let lineExists: boolean = false;
+    for (const directory of directories) {
+        try {
+            const commandPath = `${directory}/${name}`;
+            accessSync(commandPath, constants.X_OK);
+            // console.log(`${name} is ${commandPath}`);
+            return [true, commandPath];
+        } catch (e) {
+        }
+    }
+
+    return [false, ""];
+};
+
 // TODO: Uncomment the code below to pass the first stage
 rl.prompt();
 rl.on('line', (command) => {
@@ -30,21 +47,28 @@ rl.on('line', (command) => {
                 console.log(`${commandName} is a shell builtin`);
                 break;
             default:
-                const pathString: string = process.env.PATH ?? "";
-                const directories: string[] = pathString.split(path.delimiter);
-                let lineExists: boolean = false;
-                for (const directory of directories) {
-                    try {
-                        const commandPath = `${directory}/${commandName}`;
-                        accessSync(commandPath, constants.X_OK);
-                        console.log(`${commandName} is ${commandPath}`);
-                        lineExists = true;
-                        break;
-                    } catch (e) {
-                    }
-                }
+                // const pathString: string = process.env.PATH ?? "";
+                // const directories: string[] = pathString.split(path.delimiter);
+                // let lineExists: boolean = false;
+                // for (const directory of directories) {
+                //     try {
+                //         const commandPath = `${directory}/${commandName}`;
+                //         accessSync(commandPath, constants.X_OK);
+                //         console.log(`${commandName} is ${commandPath}`);
+                //         lineExists = true;
+                //         break;
+                //     } catch (e) {
+                //     }
+                // }
+                //
+                // if (!lineExists) {
+                //     console.log(`${commandName}: not found`);
+                // }
 
-                if (!lineExists) {
+                const result: [boolean, string] = findInPath(commandName);
+                if(result[0]) {
+                    console.log(`${commandName} is ${result[1]}`);
+                } else {
                     console.log(`${commandName}: not found`);
                 }
         }
@@ -56,23 +80,31 @@ rl.on('line', (command) => {
         const args: string[] = lineParts.slice(1);
         const pathString: string = process.env.PATH ?? "";
         const directories: string[] = pathString.split(path.delimiter);
-        let lineExists: boolean = false;
+        const result: [boolean, string] = findInPath(programName);
 
-        for (const directory of directories) {
-            try {
-                const commandPath = `${directory}/${programName}`;
-                accessSync(commandPath, constants.X_OK);
-                lineExists = true;
-                // execute the program
-                spawnSync(programName, args, {stdio: 'inherit'});
-                break;
-            } catch (e) {
-            }
-        }
-
-        if (!lineExists) {
+        if(result[0]) {
+            spawnSync(programName, args, {stdio: 'inherit'});
+        } else {
             console.log(`${command}: command not found`);
         }
+
+        // let lineExists: boolean = false;
+        //
+        // for (const directory of directories) {
+        //     try {
+        //         const commandPath = `${directory}/${programName}`;
+        //         accessSync(commandPath, constants.X_OK);
+        //         lineExists = true;
+        //         // execute the program
+        //         spawnSync(programName, args, {stdio: 'inherit'});
+        //         break;
+        //     } catch (e) {
+        //     }
+        // }
+        //
+        // if (!lineExists) {
+        //     console.log(`${command}: command not found`);
+        // }
     }
     rl.prompt();
 });
